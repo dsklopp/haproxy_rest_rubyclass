49fb4d38bc730b5808eba88e4a5ad8f314f60c15
git <dsklopp@gmail.com>
2014-12-28 09:53:16 -0800

    Adding core require statements to gem

--- lib/haproxy_rest.rb
+++ lib/haproxy_rest.rb
@@ -1,4 +1,8 @@
 require "haproxy_rest/version"
+require_relative 'haproxy_rest/service_control'
+require_relative 'haproxy_rest/haproxy_config'
+require_relative 'haproxy_rest/server'
+require_relative 'haproxy_rest/api'
 
 module HaproxyRest
   # Your code goes here...
