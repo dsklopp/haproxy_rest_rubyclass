905c403dc4e2493905c7b94e33dbad0a8e977b2b
git <dsklopp@gmail.com>
2015-01-11 13:35:12 -0800

    Added server with logic to restart service daemon

--- lib/haproxy_rest/server.rb
+++ lib/haproxy_rest/server.rb
@@ -6,8 +6,30 @@ DESCRIPTION: Server daemon for HAProxy, used to start and stop the daemon
 
 module HaproxyRest
   class Server
-    def initialize()
+    def initialize(config=HaproxyRest::Config.new())
+      if config.is_a? String
+        @config=HaproxyRest::Config.new(config)
+      else
+        @config=config
+      end
       puts "Initialized HaproxyRest::" + self.class.to_s
+    end
+    def backend(backend=nil)
+      if backend
+        @config.backend(backend)
+        self.restart
+      else
+        return @config.backend(backend)
+      end
+    end
+    def start()
+      `sudo service haproxy start`
+    end
+    def stop()
+      `sudo service haproxy stop`
+    end
+    def restart()
+      `sudo service haproxy restart`
     end
   end
 end
